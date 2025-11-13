d2fc148cf2ec44cc67cd7e477322872723f9906b
Mahesh Pandey <mahesh@aerion.com.au>
2025-11-13 17:00:42 +1100

    added requirements and setup files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="my_medi",
+    version="0.1.0",
+    author="Mahesh Pandey",
+    author_email="maehpandey@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+),
